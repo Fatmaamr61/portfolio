0fa165453006c74c23700d921e83db92107ed59d
fatmaamr61 <fatma.amr6197@gmail.com>
2024-11-23 12:15:09 +0200

    deploying files

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,7 +1,6 @@
-import EarthCanvas from "./Earth";
+
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
 import MainCanvas from "./Space"
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, MainCanvas };
+export { BallCanvas, ComputersCanvas, MainCanvas };
